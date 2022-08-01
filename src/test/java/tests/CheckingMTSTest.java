64044642127159d1a0e7110e0daf876133ccc7a4
Elvina Fattakhova <elvinaf1996@gmail.com>
2022-08-01 14:00:34 +0300

    new commit

--- src/test/java/tests/CheckingMTSTest.java
+++ src/test/java/tests/CheckingMTSTest.java
@@ -1,10 +1,7 @@
 package tests;
 
-import jdk.jfr.Description;
 import org.junit.Test;
 import pages.HomePage;
-
-import static com.codeborne.selenide.Selenide.switchTo;
 
 public class CheckingMTSTest {
     int countPromo = 6;
